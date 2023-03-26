a7b741e1c15c79dd946a367ffa1b4a730a4332b0
ansehyeong <dkstpgud@gmail.com>
2023-03-26 15:30:39 +0900

    fix: redis config properties 참조 변경

--- src/main/java/com/example/jpamaster/common/config/RedisTemplateConfig.java
+++ src/main/java/com/example/jpamaster/common/config/RedisTemplateConfig.java
@@ -1,7 +1,7 @@
 package com.example.jpamaster.common.config;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,19 +10,15 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@RequiredArgsConstructor
 @Configuration
 public class RedisTemplateConfig {
 
-    @Value("${spring.redis.host}")
-    private String host;
-
-    @Value("${spring.redis.port}")
-    private int port;
+    private final RedisProperties redisProperties;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort()));
     }
 
     @Bean
